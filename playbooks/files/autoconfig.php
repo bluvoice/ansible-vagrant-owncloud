3685514ee1bb15c2db6763d8c499ce9eee9bcb20
bluvoice <so.pepe88@gmail.com>
2014-05-19 13:45:20 +0200

    first commit

---
+++ playbooks/files/autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array (
+  'directory' => '/var/www/owncloud/data',
+  'dbtype' => 'mysql',
+  'dbname' => 'owncloud',
+  'dbhost' => '127.0.0.1',
+  'dbuser' => 'ownuser',
+  'dbpass' => 'ownpass',
+  'adminlogin' => 'admin',
+  'adminpass'  => 'adminp',
+);
+
